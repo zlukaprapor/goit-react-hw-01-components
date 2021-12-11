a99ca5acd500f04358ffd857ba9790f609023e66
Oleksii Tkachenko <zlukapraporr@gmail.com>
2021-12-11 15:03:52 +0200

    dz part 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import FriendList from "./component/friend-list/FriendList";
+import Profile from "./component/social-profile/Profile";
+
 import frends from "./component/friend-list/friends.json";
+import user from "./component/social-profile/user.json";
 
 function App() {
   return (
     <div>
       <FriendList frends={frends} />
+
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </div>
   );
 }
